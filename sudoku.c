df5b498a4ff9c1ce552edbcd7e52639ba371ff7f
Your Name <you@example.com>
2024-05-18 22:55:51 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -207,7 +207,7 @@ Node* DFS(Node* initial, int* cont)
 
 
 
-
+/*
 int main( int argc, char *argv[] ){
 
   Node* initial= read_file("s12a.txt");;
@@ -218,4 +218,4 @@ int main( int argc, char *argv[] ){
   print_node(final);
 
   return 0;
-}
+}*/
